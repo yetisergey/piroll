ef37e9091a78f27588109d8c236588bfae1e27ba
yetisergey <yetisergey@gmail.com>
2019-05-05 20:07:16 +0400

    github pages

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var browserSync = require('browser-sync').create();
 gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
-            baseDir: "./public"
+            baseDir: "."
         }
     });
-    gulp.watch("./public/css/*.css").on('change', browserSync.reload);
-    gulp.watch("./public/*.html").on('change', browserSync.reload);
-}); ``
+    gulp.watch("./css/*.css").on('change', browserSync.reload);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
